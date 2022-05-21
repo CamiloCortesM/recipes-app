7f589c93a4d34bf41e7a813f6c89086c373720b5
Camilo Cortes <juan.cortes05@uptc.edu.co>
2022-05-20 23:58:40 -0500

    component Navbar using boostrap

---
+++ src/components/NavRecipes.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const NavRecipes = () => {
+  return (
+    
+    <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
+  <div className="container-fluid">
+    <a className="navbar-brand">Recipes</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+        <NavLink className="nav-link " aria-current="page" to="/">Home</NavLink>
+        </li>
+        <li className="nav-item">
+        <NavLink className="nav-link " aria-current="page" to="/add">AddRecipes</NavLink>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+  );
+};
+
+export default NavRecipes;
