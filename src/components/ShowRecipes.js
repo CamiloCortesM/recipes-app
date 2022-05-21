964cbd447495cd892f807d38ffb1b96db8168036
Camilo Cortes <juan.cortes05@uptc.edu.co>
2022-05-21 01:37:20 -0500

    Screen view all Recipes add in AddRecipes component

--- src/components/ShowRecipes.js
+++ src/components/ShowRecipes.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ContextRecipes } from "../hooks/ContextRecipes";
 
 const ShowRecipes = () => {
+  const { recipes } = useContext(ContextRecipes);
+
   return (
     <div>
-      <h2>ShowRecipes</h2>
+      {recipes.map((data) => (
+        <h1>{JSON.stringify(data,null,3)}</h1>
+      ))}
     </div>
   );
 };
