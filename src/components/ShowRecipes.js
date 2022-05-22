ae64708617919c8520ce6d4cc36e3de76a1dd984
Camilo Cortes <juan.cortes05@uptc.edu.co>
2022-05-21 23:16:57 -0500

    create Card Recipes

--- src/components/ShowRecipes.js
+++ src/components/ShowRecipes.js
@@ -3,12 +3,29 @@ import { ContextRecipes } from "../hooks/ContextRecipes";
 
 const ShowRecipes = () => {
   const { recipes } = useContext(ContextRecipes);
+  console.log(recipes)
 
   return (
-    <div>
-      {recipes.map((data) => (
+    <div className="Container-Card">
+      {recipes.map((data) => {
+        return (
+          <div className="Card-Recipes">
+            <div className="Card-Img">
+            <img src={data.img}/>
+            </div>
+          <div className="Card-Name">
+          <h2>{data.name}</h2>
+          </div>
+          <div className="Card-Fotter">
+          <p>{data.descrip}</p>
+          </div>
+          </div>
+        );
+      })}
+     
+      {/* {recipes.map((data) => (
         <h1>{JSON.stringify(data,null,3)}</h1>
-      ))}
+      ))} */}
     </div>
   );
 };
