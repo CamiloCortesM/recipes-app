c8f7058f4ca16118415dde2caecede8f67c1d1a7
Camilo Cortes <juan.cortes05@uptc.edu.co>
2022-05-22 01:55:49 -0500

    the data with which the program is started is created and is returned

---
+++ src/helpers/initialData.js
@@ -0,0 +1,39 @@
+export const initialData = ()=>{
+    return [
+        {
+          id: 1653199702039,
+          name: "Pizza de Peperoni",
+          ingredients:
+            "100 gramos de harina blanca,2.5 gramos de levadura seca,1/2 cucharaditas de azúcar,1/2 cucharaditas de aceite de oliva extra virgen,1 pizca de sal de mesa,21 gramos de pasta de tomate,10 mililitros de aceite de oliva extra virgen,1/4 dientes de ajo molido,1 pizca de sal de mesa,1 pizca de pimienta en polvo,1 pizca de albahaca,1 pizca de romero seco,1/2 tazas de queso rallado,1/2 tazas de peperoni",
+          img: "https://placeralplato.com/files/2019/02/Pizza-de-pepperoni.jpg",
+          descrip:
+            "Poner la harina en un bol y hacerlo un hoyo en el centro, Agregar ahí mismo la levadura, azúcar, sal, y 50 mililitros de agua tibia,Mezclar todos los ingredientes hasta formar una masa,Espolvorear harina en,una superficie plana y poner ahi la masa,Agregar el aceite y amasar hasta que este suave,Agarra otro bol y espolvorea un poco de harina y poner la masa dentro del bol,Dejar reposar la masa por 1 hora en un lugar calido,Ponerla otra vez en una superficie plana y estirarla con el rodillo\nDarle forma a la masa y ponerla en una bandeja,Para la salsa juntar la pasta de tomate, 40 mililitros de agua, el aceite, ajo, sal y pimienta al gusto y las especies.,Ya que esta la masa, agregar la salsa y extenderla por toda la masa y arriba de la salsa agregar el queso.,Agregar el peperoni arriba del queso y meterla al horno por 14 minutos",
+        },
+        {
+          id: 1653200348952,
+          name: "Hamburguesa árabe",
+          ingredients:
+            "2 tazas de col finamente rebanada 2 cdas, de aceite de oliva,1 jitomate rebanado,1/3 de taza de perejil picado,2 cdas. de jugo de limón,400 g de carne molida,1/2 cda. de ajo en polvo,1/4 de taza de cebolla picada,1 cda. de orégano,1 cda. de aceite vegetal,1 huevo,2 cdas. de pan molido,5 cdas. de jocoque ,1 cda. de zatar,4 piezas de pan árabe",
+          img: "https://dam.cocinafacil.com.mx/wp-content/uploads/2019/05/hamburguesa-arabe.png",
+          descrip:
+            "Mezcla la carne con el ajo, el huevo, el pan molido, la cebolla y el orégano. Salpimienta.\nForma tortitas de carne pequeñas y ásalas en una sartén con el aceite a fuego medio.\nMezcla el resto de los ingredientes (excepto el pan) y salpimienta al gusto.\nCalienta las piezas de pan y ábrelas con cuidado. Embárralas con jocoque, rellena con la ensalada y carne. Termina con un poco de zatar y sirve.",
+        },
+        {
+          id: 1653200571473,
+          name: "Jugo Verde",
+          ingredients: "Kale,pepino,Apio,Limon,Manzana verde,jengibre",
+          img: "https://d1kxxrc2vqy8oa.cloudfront.net/wp-content/uploads/2019/08/08122640/RFB-3107-6-jugoverde.jpg",
+          descrip:
+            "Hacer un jugo verde en casa es muy muy fácil. Lo único que necesitas es un poco de práctica para que te salgan perfectos.\n\nLo primero es lavar super bien las frutas y verduras, los ingredientes para jugo verde\nNo importa si usas verduras orgánicas o convencionales…. SIEMPRE lávalas. Es mejor.\nCuando yo uso verduras orgánicas no las pelo, si no son orgánicas si. Por ejemplo, cuando tomé las fotos para esta receta de jugo verde pelé el pepino porque no era orgánico. Haz lo mismo con el resto de los ingredientes.\n\n¿Cómo preparar los ingredientes del jugo verde?\nEste paso depende mucho del juicer (affiliate link) ( juicer/extractor de jugos (affiliate link)) que tengas. Hay maquinas para hacer jugo que pueden con todo, hay otras que no. Yo por si las dudas trato de cortar todo en pedazos chicos. Siento que es como mejor trabajan las máquinas y como mas le sacan jugo a todos los ingredientes que uses.\n\n¿Cómo hacer jugo verde?\nHay dos tipos de extractores, uno de centrifugado y otro de extracción en frío (slow juicer (affiliate link)). Cualquiera de los dos es bueno, todo depende del presupuesto, el espacio que tengas en casa para guardarlos y del tipo de verduras que vayas a usar.\nCon cualquiera de los dos extractores, yo paso todo revuelto, paso el pepino y luego el apio y así, Es mejor cuando todo se pasa revuelto porque así si algo se queda en el extractor, el siguiente ingrediente le ayuda a pasar.\nAhora, si tienes un extractor de centrifugado, puede ser que no deje tan seca la pulpa. En este caso, vuélvela a pasar por el extractor.",
+        },
+        {
+          id: 1653200892491,
+          name: "Lasaña",
+          ingredients:
+            "1 cucharada de aceite (14 g), 1 diente de ajo pelado y finamente picado (3 g) ,1 pimentón rojo sin semilla y cortado en cubos pequeños (120 g) ,1 cebolla cabezona blanca, finamente picada (80 g), 2 tomates rojos medianos sin piel, finamente picados (240 g), 1 sobre de CALDO CON COSTILLA MAGGI® DESMENUZADO (9 g) ,1 libra de carne molida magra (500 g), 1 1/2 tazas de agua (375 ml) ,1/2 caja de pasta para lasaña precocida (250 g) ,8 lonjas de queso mozzarella (136 g) ,1 sobre de BASE PARA SALSA BECHAMEL MAGGI® (50 g) ,1 taza de leche (250 ml) ,1 taza de agua (250 ml) ,4 cucharadas de queso parmesano rallado (20 g)",
+          img: "https://www.cocinavital.mx/wp-content/uploads/2021/06/lasana-bolonesa-con-parmesano-634x420.jpg",
+          descrip:
+            "PASO 1: Precalienta el horno\nPrecalienta el horno a 180°C/ 360°F 30 minutos antes de la preparación.\n\nPASO 2: Prepara la carne\nCalienta el aceite en una sartén a fuego medio durante 3 minutos; luego sofríe el ajo, el pimentón, la cebolla y el tomate. Añade el sobre de CALDO CON COSTILLA MAGGI® DESMENUZADO y cocina por 2 minutos. Adiciona la carne y sofríe por 5 minutos sin dejar de remover, luego agrega el agua hasta que hierva y la carne se cocine.\nRetira del fuego y reserva hasta el momento de usar\n\nPASO 3: Pasa la pasta por agua tibia\nPasa la pasta para lasaña por agua tibia con el fin de que no se parta en el momento de armarla.\n\nPASO 4: Haz la salsa\nDisuelve el contenido del sobre de BASE PARA SALSA BECHAMEL MAGGI® en una taza de agua fría y una taza de leche, mezcla hasta disolver bien. Lleva a fuego medio en una olla por 5 minutos sin dejar de remover, hasta que hierva y espese. Retira del fuego.\n\nPASO 5: Arma la lasaña\nPon en el fondo de la refractaria o molde una cama de carne, encima las láminas de pasta, la mitad de la salsa y una cama de queso mozzarella. Repite estos pasos hasta terminar los ingredientes, asegurando que la última capa sea de pasta y salsa. Por último, espolvorea el queso parmesano encima.\n\n\nLleva la refractaria o molde al horno y déjala por 10 a 15 minutos aproximadamente o hasta gratinar.\n\nPASO 6: Sirve\nRetira del horno y sirve",
+        },
+      ]
+}
