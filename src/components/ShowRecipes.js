b6e1d986a97700ed0c2a3db2d0f91b1560d92128
Camilo Cortes <juan.cortes05@uptc.edu.co>
2022-05-22 01:54:07 -0500

    was create a new component in map with a data and key

--- src/components/ShowRecipes.js
+++ src/components/ShowRecipes.js
@@ -1,31 +1,15 @@
 import React, { useContext } from "react";
 import { ContextRecipes } from "../hooks/ContextRecipes";
+import CardRecipes from "./CardRecipes";
 
 const ShowRecipes = () => {
   const { recipes } = useContext(ContextRecipes);
-  console.log(recipes)
 
   return (
     <div className="Container-Card">
-      {recipes.map((data) => {
-        return (
-          <div className="Card-Recipes">
-            <div className="Card-Img">
-            <img src={data.img}/>
-            </div>
-          <div className="Card-Name">
-          <h2>{data.name}</h2>
-          </div>
-          <div className="Card-Fotter">
-          <p>{data.descrip}</p>
-          </div>
-          </div>
-        );
-      })}
-     
-      {/* {recipes.map((data) => (
-        <h1>{JSON.stringify(data,null,3)}</h1>
-      ))} */}
+      {recipes.map((data) => (
+        <CardRecipes key={data.id}data={data}/>
+      ))}
     </div>
   );
 };
