1042a85bc3c9a61b80ec6a2e43869e7f861e400d
Camilo Cortes <juan.cortes05@uptc.edu.co>
2022-05-22 01:51:25 -0500

    the data was initialized with the localstorage or with a function

--- src/MainApp.js
+++ src/MainApp.js
@@ -1,9 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import RoutesApp from "./components/RoutesApp";
+import { initialData } from "./helpers/initialData";
 import { ContextRecipes } from "./hooks/ContextRecipes";
 
+const init = () => {
+  return (
+    JSON.parse(localStorage.getItem("recipes")) || initialData()
+  )
+};
 const MainApp = () => {
-  const [recipes, setRecipes] = useState([]);
+  const [recipes, setRecipes] = useState(init);
+
+  useEffect(() => {
+    localStorage.setItem("recipes", JSON.stringify(recipes));
+  }, [recipes]);
+
   return (
     <div>
       <ContextRecipes.Provider
