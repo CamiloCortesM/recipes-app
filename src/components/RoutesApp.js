80b3cd1bd4d81ed0dcd164942637c6fef62babfe
Camilo Cortes <juan.cortes05@uptc.edu.co>
2022-05-23 01:10:10 -0500

    was configured the initialdata and also the routes

--- src/components/RoutesApp.js
+++ src/components/RoutesApp.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route,BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import AddRecipes from "./AddRecipes";
 import Error from "./Error";
 import NavRecipes from "./NavRecipes";
@@ -7,19 +7,17 @@ import SceneRecipe from "./SceneRecipe";
 import ShowRecipes from "./ShowRecipes";
 
 const RoutesApp = () => {
-
-
   return (
     <Router>
       <div>
         <NavRecipes />
         <div className="container">
-        <Routes>
-          <Route path="/add" element={<AddRecipes />} />
-          <Route path="/" element={<ShowRecipes />} />
-          <Route path="*" element={<Error />} />
-          <Route path="/recipes/:id" element={<SceneRecipe />} />
-        </Routes>
+          <Routes>
+            <Route path="/add" element={<AddRecipes />} />
+            <Route path="/" element={<ShowRecipes />} />
+            <Route path="*" element={<Error />} />
+            <Route path="/recipes/:id" element={<SceneRecipe />} />
+          </Routes>
         </div>
       </div>
     </Router>
