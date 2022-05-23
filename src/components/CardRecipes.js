778e93fef73e00579511855b07c72053d7c35d98
Camilo Cortes <juan.cortes05@uptc.edu.co>
2022-05-22 21:02:36 -0500

    was Created a Card-recipe and send id with in params

--- src/components/CardRecipes.js
+++ src/components/CardRecipes.js
@@ -1,15 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const CardRecipes = ({ data }) => {
+
   return (
     <div className="Card-Recipes">
-      <div className="Card-Img">
-        <img src={data.img} alt={data.name} />
-      </div>
-      <div className="Card-Name">
-        <h2>{data.name}</h2>
-      </div>
-      <div className="Card-Fotter">{data.descrip}</div>
+      <Link to={`/recipes/${data.id}`}  style={{ textDecoration: 'none', color:'#000' }}>
+        <div className="Card-Img">
+          <img src={data.img} alt={data.name} />
+        </div>
+        <div className="Card-Name">
+          <h2>{data.name}</h2>
+        </div>
+        <div className="Card-Fotter">{data.descrip}</div>
+      </Link>
     </div>
   );
 };
