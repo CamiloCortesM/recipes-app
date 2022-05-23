5e11330eabdde0365a4cc4a16af182498fe35f61
Camilo Cortes <juan.cortes05@uptc.edu.co>
2022-05-22 21:05:58 -0500

    was created /recipes/:id route in component SceneRecipe

--- src/components/RoutesApp.js
+++ src/components/RoutesApp.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { Route,BrowserRouter as Router, Routes } from "react-router-dom";
 import AddRecipes from "./AddRecipes";
+import Error from "./Error";
 import NavRecipes from "./NavRecipes";
+import SceneRecipe from "./SceneRecipe";
 import ShowRecipes from "./ShowRecipes";
 
 const RoutesApp = () => {
+
+
   return (
     <Router>
       <div>
@@ -13,6 +17,8 @@ const RoutesApp = () => {
         <Routes>
           <Route path="/add" element={<AddRecipes />} />
           <Route path="/" element={<ShowRecipes />} />
+          <Route path="*" element={<Error />} />
+          <Route path="/recipes/:id" element={<SceneRecipe />} />
         </Routes>
         </div>
       </div>
