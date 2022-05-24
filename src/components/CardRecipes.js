3e710ce627dd46fe211b4224a985c718f5fda31f
Camilo Cortes <juan.cortes05@uptc.edu.co>
2022-05-24 13:33:22 -0500

    was optimized

--- src/components/CardRecipes.js
+++ src/components/CardRecipes.js
@@ -2,10 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const CardRecipes = ({ data }) => {
-
   return (
     <div className="Card-Recipes">
-      <Link to={`/recipes/${data.id}`}  style={{ textDecoration: 'none', color:'#000' }}>
+      <Link
+        to={`/recipes/${data.id}`}
+        style={{ textDecoration: "none", color: "#000" }}
+      >
         <div className="Card-Img">
           <img src={data.img} alt={data.name} />
         </div>
--- src/components/NavRecipes.js
+++ src/components/NavRecipes.js
@@ -3,25 +3,36 @@ import { NavLink } from "react-router-dom";
 
 const NavRecipes = () => {
   return (
-    
-    <nav className="navbar navbar-expand-lg navbar-dark" >
-  <div className="container-fluid expand">
-    <h2 className="navbar-brand">Recipes</h2>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-        <NavLink className="nav-link " aria-current="page" to="/">Home</NavLink>
-        </li>
-        <li className="nav-item">
-        <NavLink className="nav-link " aria-current="page" to="/add">AddRecipes</NavLink>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+    <nav className="navbar navbar-expand-lg navbar-dark">
+      <div className="container-fluid expand">
+        <h2 className="navbar-brand">Recipes</h2>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavLink className="nav-link " aria-current="page" to="/">
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link " aria-current="page" to="/add">
+                AddRecipes
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 };
 
