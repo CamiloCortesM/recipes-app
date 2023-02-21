cadc3247e1fd5e428af6a87b7045c0085769ae8e
Camilo Cortes <67564515+CamiloCortesM@users.noreply.github.com>
2023-02-21 16:42:40 -0500

    fix paths

--- src/helpers/initialData.js
+++ src/helpers/initialData.js
@@ -5,7 +5,7 @@ export const initialData = ()=>{
           name: "Pizza de Peperoni",
           ingredients:
             "100 gramos de harina blanca,2.5 gramos de levadura seca,1/2 cucharaditas de azúcar,1/2 cucharaditas de aceite de oliva extra virgen,1 pizca de sal de mesa,21 gramos de pasta de tomate,10 mililitros de aceite de oliva extra virgen,1/4 dientes de ajo molido,1 pizca de sal de mesa,1 pizca de pimienta en polvo,1 pizca de albahaca,1 pizca de romero seco,1/2 tazas de queso rallado,1/2 tazas de peperoni",
-          img: "https://placeralplato.com/files/2019/02/Pizza-de-pepperoni.jpg",
+          img: "https://www.lanacion.com.ar/resizer/gpObK_-cwpTLdqML9IJgu_MJKI0=/1200x800/filters:format(webp):quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/OSJYHXRML5DUTEMJRYUCZTIALI.jpg",
           descrip:
             "Poner la harina en un bol y hacerlo un hoyo en el centro, Agregar ahí mismo la levadura, azúcar, sal, y 50 mililitros de agua tibia,Mezclar todos los ingredientes hasta formar una masa,Espolvorear harina en,una superficie plana y poner ahi la masa,Agregar el aceite y amasar hasta que este suave,Agarra otro bol y espolvorea un poco de harina y poner la masa dentro del bol,Dejar reposar la masa por 1 hora en un lugar calido,Ponerla otra vez en una superficie plana y estirarla con el rodillo\n\nDarle forma a la masa y ponerla en una bandeja,Para la salsa juntar la pasta de tomate, 40 mililitros de agua, el aceite, ajo, sal y pimienta al gusto y las especies.,Ya que esta la masa, agregar la salsa y extenderla por toda la masa y arriba de la salsa agregar el queso.,Agregar el peperoni arriba del queso y meterla al horno por 14 minutos",
         },
@@ -14,7 +14,7 @@ export const initialData = ()=>{
           name: "Hamburguesa árabe",
           ingredients:
             "2 tazas de col finamente rebanada 2 cdas, de aceite de oliva,1 jitomate rebanado,1/3 de taza de perejil picado,2 cdas. de jugo de limón,400 g de carne molida,1/2 cda. de ajo en polvo,1/4 de taza de cebolla picada,1 cda. de orégano,1 cda. de aceite vegetal,1 huevo,2 cdas. de pan molido,5 cdas. de jocoque ,1 cda. de zatar,4 piezas de pan árabe",
-          img: "https://dam.cocinafacil.com.mx/wp-content/uploads/2019/05/hamburguesa-arabe.png",
+          img: "https://www.infobae.com/new-resizer/sh0cQBavobeT-OvaLzu-VP5mi5A=/992x558/filters:format(webp):quality(85)/arc-anglerfish-arc2-prod-infobae.s3.amazonaws.com/public/FJKXKQKMMJBV7KQ7XQ3YNFO7LU.jpg",
           descrip:
             "Mezcla la carne con el ajo, el huevo, el pan molido, la cebolla y el orégano. Salpimienta.\n\nForma tortitas de carne pequeñas y ásalas en una sartén con el aceite a fuego medio.\n\nMezcla el resto de los ingredientes (excepto el pan) y salpimienta al gusto.\n\nCalienta las piezas de pan y ábrelas con cuidado. Embárralas con jocoque, rellena con la ensalada y carne. Termina con un poco de zatar y sirve.",
         },
